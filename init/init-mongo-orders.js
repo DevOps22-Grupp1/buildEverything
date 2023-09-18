d98e4729697e6c15666fed9f507d91d757bb4843
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-09-18 21:06:51 +0200

    deleted mongo data folders, and added gitignore, added init mongo JS files

---
+++ init/init-mongo-orders.js
@@ -0,0 +1,44 @@
+db.createUser({
+  user: "sudo_admin",
+  pwd: "password",
+  roles: [
+    {
+      role: "readWrite",
+      db: "allOrders",
+    },
+  ],
+});
+
+db.createCollection("orders");
+db.orders.insertMany([
+  {
+    id: 1,
+    userid: 1,
+    productid: 1,
+  },
+  {
+    id: 2,
+    userid: 2,
+    productid: 3,
+  },
+  {
+    id: 3,
+    userid: 3,
+    productid: 2,
+  },
+  {
+    id: 4,
+    userid: 4,
+    productid: 5,
+  },
+  {
+    id: 5,
+    userid: 5,
+    productid: 6,
+  },
+  {
+    id: 6,
+    userid: 6,
+    productid: 4,
+  },
+]);
