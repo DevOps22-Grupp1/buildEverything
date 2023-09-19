88ce1eb2fa245c49fde98f3299eb0da5bf807ad8
DennisNevback <dennis.nevback@gmail.com>
2023-09-19 10:59:14 +0200

    Added instructions in README

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # buildEverything
+Start by running "bash start.sh"
+Stop by running "bash stop.sh"
 
 **Medlemmar i grupp 1 med github alias:** 
 - Jarl | alias: webcrunch
--- start.sh
+++ start.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 cd home-page-service
 git pull https://github.com/DevOps22-Grupp1/home-page-service prometheus-fix
+docker-compose pull
 docker compose up --detach
 cd ..
 cd order-processing-service
 git pull https://github.com/DevOps22-Grupp1/order-processing-service project-merge
+docker-compose pull
 docker compose up --detach
 cd ..
 cd produkt-catalog-service
 git pull https://github.com/DevOps22-Grupp1/produkt-catalog-service project-merge
+docker-compose pull
 docker compose up --detach
 cd ..
 
