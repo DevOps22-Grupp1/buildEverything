68dc26485e079a92df89b984739a22c60a525f72
DennisNevback <dennis.nevback@gmail.com>
2023-09-20 13:39:12 +0200

    update to pull from main

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 cd home-page-service
-git pull https://github.com/DevOps22-Grupp1/home-page-service prometheus-fix
+git pull https://github.com/DevOps22-Grupp1/home-page-service main
 docker-compose pull
 docker compose up --detach
 cd ..
 cd order-processing-service
-git pull https://github.com/DevOps22-Grupp1/order-processing-service project-merge
+git pull https://github.com/DevOps22-Grupp1/order-processing-service main
 docker-compose pull
 docker compose up --detach
 cd ..
 cd produkt-catalog-service
-git pull https://github.com/DevOps22-Grupp1/produkt-catalog-service project-merge
+git pull https://github.com/DevOps22-Grupp1/produkt-catalog-service main
 docker-compose pull
 docker compose up --detach
 cd ..
