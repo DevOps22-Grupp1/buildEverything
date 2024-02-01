9bfce3afe5096d5592d1f536e3bb7a458719fa01
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2024-02-01 15:28:01 +0100

    Update productpage-spec.cy.js

--- cypress/e2e/1-getting-started/productpage-spec.cy.js
+++ cypress/e2e/1-getting-started/productpage-spec.cy.js
@@ -7,9 +7,9 @@ describe('Navigation Tests', () => {
     cy.contains('a', 'Products').click();
 
     // Log information about the element using cy.get and cy.log
-    cy.get('a').contains('Products').then(($el) => {
-      cy.log(`Found ${$el.length} element(s) matching the selector.`);
-    });
+    //cy.get('a').contains('Products').then(($el) => {
+     // cy.log(`Found ${$el.length} element(s) matching the selector.`);
+   // });
 
     // Verifing the URL includes '/products'
     cy.url().should('include', '/products');
