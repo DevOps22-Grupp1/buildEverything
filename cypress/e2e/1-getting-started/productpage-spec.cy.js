662348a56d666453e98dbbbbf6fc494df9affc3a
harisha7 <harisha.srinivas74@gmail.com>
2024-02-02 13:38:39 +0100

    updated

--- cypress/e2e/1-getting-started/productpage-spec.cy.js
+++ cypress/e2e/1-getting-started/productpage-spec.cy.js
@@ -1,19 +1,12 @@
 describe('Navigation Tests', () => {
-  it('should navigate to Products page', () => {
+  it('should display Products on the page after clicking Products link', () => {
     // Visit the base URL
     cy.visit('/');
 
     // Clicking on the element using cy.contains
     cy.contains('a', 'Products').click();
 
-    // Log information about the element using cy.get and cy.log
-    cy.get('a').contains('Products').then(($el) => {
-      cy.log(`Found ${$el.length} element(s) matching the selector.`);
-    });
-
-    // Verifing the URL includes '/products'
-    cy.url().should('include', '/products');
+    // Assertion: Check if the "Products" text is present on the page
+    cy.contains('Products').should('be.visible');
   });
 });
-
-
