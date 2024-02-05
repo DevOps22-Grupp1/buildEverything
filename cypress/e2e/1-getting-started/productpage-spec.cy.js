cbdf85bd25583d71c83ecafed01d0fe8be47fb07
harisha7 <harisha.srinivas74@gmail.com>
2024-02-05 13:48:04 +0100

    added new test

--- cypress/e2e/1-getting-started/productpage-spec.cy.js
+++ cypress/e2e/1-getting-started/productpage-spec.cy.js
@@ -1,12 +1,12 @@
-describe('Navigation Tests', () => {
+ describe('Navigation Tests', () => {
   it('should display Products on the page after clicking Products link', () => {
     // Visit the base URL
     cy.visit('/');
 
-    // Clicking on the element using cy.contains
+/*    // Clicking on the element using cy.contains
     cy.contains('a', 'Products').click();
 
     // Assertion: Check if the "Products" text is present on the page
-    cy.contains('Products').should('be.visible');
+    cy.contains('Products').should('be.visible'); */
   });
 });
