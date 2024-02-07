4625ceac006633a19d6bc1cf0950a0ea6f776142
harisha7 <harisha.srinivas74@gmail.com>
2024-02-07 14:59:05 +0100

    minimal test step 2

--- cypress/e2e/1-getting-started/produktpage-spec.cy.js
+++ cypress/e2e/1-getting-started/produktpage-spec.cy.js
@@ -1,10 +1,10 @@
 describe('Product Page Test', () => {
  it('should display Products on the page after clicking Products link', () => {
     cy.visit('/');
-    /*// Clicking on the element using cy.contains for Products link
+    // Clicking on the element using cy.contains for Products link
     cy.contains('a', 'Products').click();
 
-    // Assertion: Check if the URL contains "/products"
+    /*// Assertion: Check if the URL contains "/products"
     cy.url().should('include', '/products');
 
     // Assertion: Check if the "Products" text is present on the page
