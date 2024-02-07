82f9cb8277731beb5e52feb025093fb0159cc756
harisha7 <harisha.srinivas74@gmail.com>
2024-02-07 13:45:11 +0100

    checking test again

--- cypress/e2e/1-getting-started/productpage-spec.cy.js
+++ cypress/e2e/1-getting-started/productpage-spec.cy.js
@@ -1,24 +1,24 @@
-describe('Navigation Tests', () => {
+describe('Product Page Test', () => {
   it('should return a 200 status code and contain "Products"', () => {
+    // Make a request to the product page
     cy.request('/products')
       .its('status')
       .should('equal', 200);
 
+    // Make a request to the product page
     cy.request('/products')
       .its('body')
-      .should('include', 'Products');  
-  }); 
-});  
+      .should('include', 'Products');
+  });
 
-
-
+  it('should display Products on the page after clicking Products link', () => {
     // Visit the base URL
-    //cy.visit('/');
+    cy.visit('/');
 
-   // Clicking on the element using cy.contains
-    //cy.contains('a', 'Products').click();
+    // Clicking on the element using cy.contains for Products link
+    cy.contains('a', 'Products').click();
 
     // Assertion: Check if the "Products" text is present on the page
-    //cy.contains('Products').should('be.visible'); */
-
-
+    cy.contains('Products').should('be.visible');
+  });
+});
