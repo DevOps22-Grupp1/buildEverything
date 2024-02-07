cb50a87b04a94195e3f7b1c3b61424c44717ebc7
harisha7 <harisha.srinivas74@gmail.com>
2024-02-07 09:31:04 +0100

    update test

--- cypress/e2e/1-getting-started/productpage-spec.cy.js
+++ cypress/e2e/1-getting-started/productpage-spec.cy.js
@@ -3,10 +3,10 @@
     // Visit the base URL
     cy.visit('/');
 
-/*    // Clicking on the element using cy.contains
+   // Clicking on the element using cy.contains
     cy.contains('a', 'Products').click();
 
     // Assertion: Check if the "Products" text is present on the page
-    cy.contains('Products').should('be.visible'); */
+    //cy.contains('Products').should('be.visible'); */
   });
 });
