48b2c12c02de6000101de2ae31a6c1f8cfe30304
harisha7 <harisha.srinivas74@gmail.com>
2024-02-07 14:00:44 +0100

    last chance

---
+++ cypress/e2e/1-getting-started/produktpage-spec.cy.js
@@ -0,0 +1,15 @@
+describe('Product Page Test', () => {
+  it('should display Products on the page after clicking Products link', () => {
+    // Visit the base URL
+    cy.visit('/');
+
+    // Clicking on the element using cy.contains for Products link
+    cy.contains('a', 'Products').click();
+
+    // Assertion: Check if the URL contains "/products"
+    cy.url().should('include', '/products');
+
+    // Assertion: Check if the "Products" text is present on the page
+    cy.contains('Products').should('be.visible');
+  });
+});
